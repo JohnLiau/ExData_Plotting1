2f7714afd964f8df987d16c1e1b7dad1a9169a5a
JohnLiau <Liau-Family@USA.net>
2014-08-10 15:31:45 -0700

    Create plot4.R

---
+++ plot4.R
@@ -0,0 +1,24 @@
+plot4 <- function() {
+    ## Read input file
+    fulldata<-read.table(unz("~/Learn/Data Science/JHU04 Exploratory Data Analysis/exdata-data-household_power_consumption.zip","household_power_consumption.txt"),header=TRUE,sep=';')
+    colnames(fulldata)<-gsub("_",".",colnames(fulldata))
+    Date<-as.Date(fulldata$Date,"%d/%m/%Y")
+    data<-fulldata[Date>=as.Date('1/2/2007',"%d/%m/%Y") & Date<=as.Date('2/2/2007',"%d/%m/%Y"),]
+    data$Time<-strptime(paste(data$Date,data$Time),"%d/%m/%Y %H:%M:%S")
+    data$Global.active.power<-as.numeric(levels(data$Global.active.power))[data$Global.active.power]
+    data$Voltage<-as.numeric(levels(data$Voltage))[data$Voltage]
+    data$Global.reactive.power<-as.numeric(levels(data$Global.reactive.power))[data$Global.reactive.power]
+    par(mfcol=c(2,2))
+    with(data,{
+        plot(Time,Global.active.power,bg=NA,type='l',xlab="",ylab="Global Active Power(kilowatts)",cex.axis=0.7,cex.lab=0.65,font.axis=1,font.lab=1)
+        plot(Time,Sub.metering.1,bg=NA,type='n',col='black',xlab="",ylab="Energy sub metering",cex.axis=0.7,cex.lab=0.65,font.axis=1,font.lab=1)
+        legend("topright",lty=1,box.col="transparent",col=c("black","red","blue"),legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),cex=0.55)
+        points(Time,Sub.metering.1,bg=NA,type='l',col='black',xlab="",ylab="Energy sub metering",cex.axis=0.7,cex.lab=0.65,font.axis=1,font.lab=1)
+        points(Time,Sub.metering.2,bg=NA,type='l',col='red',xlab="",ylab="Energy sub metering",cex.axis=0.7,cex.lab=0.65,font.axis=1,font.lab=1)
+        points(Time,Sub.metering.3,bg=NA,type='l',col='blue',xlab="",ylab="Energy sub metering",cex.axis=0.7,cex.lab=0.65,font.axis=1,font.lab=1)
+        plot(Time,Voltage,bg=NA,type='l',xlab="datetime",ylab="Voltage",cex.axis=0.7,cex.lab=0.65,font.axis=1,font.lab=1)
+        plot(Time,Global.reactive.power,bg=NA,type='l',xlab="datetime",ylab="Global_reactive_power",cex.axis=0.7,cex.lab=0.65,font.axis=1,font.lab=1)
+    })
+    dev.copy(png,file='~/Learn/Data Science/JHU04 Exploratory Data Analysis/plot4.png')
+    dev.off()
+}
